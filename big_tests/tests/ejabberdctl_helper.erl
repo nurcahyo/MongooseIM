e3b3ee7512aeada67961d2c2ed142acac80347e2
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-03-26 15:00:55 +0200

    Fix issues cause by function extraction

--- big_tests/tests/ejabberdctl_helper.erl
+++ big_tests/tests/ejabberdctl_helper.erl
@@ -52,3 +52,7 @@ loop(Port, Data, Timeout) ->
     after Timeout ->
         throw(timeout)
     end.
+
+mongooseim_script(Node, Cmd, Args, Config) ->
+    CtlCmd = distributed_helper:script_path(Node, Config, "mongooseim"),
+    run(string:join([CtlCmd, Cmd | ejabberdctl_helper:normalize_args(Args)], " ")).
